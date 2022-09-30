32b8868a38a04f7415b17d29776fec6a43c9128d
HanYouyang <99865255+tomPundi@users.noreply.github.com>
2022-09-30 11:33:10 +0800

    41 strings

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,4 +1,7 @@
+use crate::http::request;
+
 use super::method::Method;
+use std::str::Utf8Error;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::Debug;
@@ -18,9 +21,27 @@ impl TryFrom<&[u8]> for Request {
 
     // GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        
+        let request = str::from_utf8(buf)?;
+        
+        
+        
+        
+        
         unimplemented!();
     }
 }
+
+fn get_next_word(request: &str) -> Option<(&str,&str)> {
+    for (i, c) in request.chars().enumerate() {
+        if c == " " {
+            return Some((&request[..i], &request[i+1..]));
+        }
+    }
+
+    None
+}
+
 
 pub enum ParseError {
     InvalidRequest,
@@ -37,6 +58,12 @@ impl ParseError {
             Self::InvalidProtocol => "Invalid Protocol",
             Self::InvalidMethod => "Invalid Method",
         }
+    }
+}
+
+impl Utf8Error for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
     }
 }
 
