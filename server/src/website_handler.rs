498f3c776e1568fcd08d24b591a79fd039b00a79
HanYouyang <99865255+tomPundi@users.noreply.github.com>
2022-10-02 20:40:05 +0800

    60 security

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -17,7 +17,20 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        // fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+
+
     }
 }
 
@@ -32,7 +45,10 @@ impl Handler for WebsiteHandler {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
                 "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
-                _ => Response::new(StatusCode::NotFound, None),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             
             }
             _ => Response::new(StatusCode::NotFound, None),
